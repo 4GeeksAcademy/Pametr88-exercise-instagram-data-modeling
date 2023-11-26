af141a4cdf3e55ded181253a55ba5ff1afaaebe5
Pamela <140130365+Pametr88@users.noreply.github.com>
2023-11-26 16:36:45 +0000

    proyecto finalizado

--- src/models.py
+++ src/models.py
@@ -7,23 +7,54 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)      #id de las tablas tiene que ser verdader
+    name = Column(String(50), nullable=False)  #no puede ser null obligatorio
+    email = Column(String(50), nullable=False) #(50) caracteres restringidos
+    password = Column(String(250), nullable=False) #(250) para que cueste hackear
+    
+class Follower(Base):
+    __tablename__ = 'follower'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    photo = Column(String(250))
+    comment = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Post(Base):           #en mayuscula
+    __tablename__ = 'post'  # en minuscula
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    photo = Column(String(250))
+    comment = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Media(Base):           #en mayuscula
+    __tablename__ = 'media'  # en minuscula
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    photo = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Comment(Base):           #en mayuscula
+    __tablename__ = 'comment'  # en minuscula
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     def to_dict(self):
         return {}
